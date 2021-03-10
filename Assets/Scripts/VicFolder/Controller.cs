eef77aae6b2a4aa14d9647c112600bf77b671b87
Victorpetts <vike.pet@hotmail.com>
2021-03-10 21:29:59 +0100

    Squares light up now

--- Assets/Scripts/VicFolder/Controller.cs
+++ Assets/Scripts/VicFolder/Controller.cs
@@ -1,17 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Controller : MonoBehaviour {
     public PlayerModel playerModel;
     public int cost = 20;
 
+    public int[] table = { 90, 6, 2, 1, 1 };
+    public List<GameObject> buttons;
+
     public void RollGacha() {
         if (!playerModel.HasEnoughGold(cost)) return;
         playerModel.Gold -= cost;
         playerModel.Inventory.Add(new Item() { Rarity = "Legendary" });
-        Debug.Log($"You did a roll and have {playerModel.Gold} gold left!");
         ShowAllItems();
         // foreach(var item in GenerateRandomLoot(lootBoxConfig))
         //     this.playerModel.Items.Add(item);
+        foreach(var button in buttons)
+            button.GetComponent<Image>().color = Color.grey;
+        GetRandomItem();
+    }
+    
+    private void GetRandomItem() {
+        var randomNumb = Random.Range(0, 100);
+        for (int i = 0; i < table.Length; i++) {
+            if (randomNumb <= table[i]) {
+                buttons[i].GetComponent<Image>().color = Color.magenta;
+                return;
+            } else {
+                randomNumb -= table[i];
+            }
+        }
     }
 
     private void ShowAllItems() {

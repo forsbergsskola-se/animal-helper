d6aca9d99b8368677afabdbd9d7b72c83771c288
Victorpetts <vike.pet@hotmail.com>
2021-03-12 14:26:25 +0100

    Added swipe in the start of the race and adjusted speed and drag values

--- Assets/Scripts/JosefsFolder/Script/MoveCar.cs
+++ Assets/Scripts/JosefsFolder/Script/MoveCar.cs
@@ -1,39 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MoveCar : MonoBehaviour
-{
+public class MoveCar : MonoBehaviour {
     public Text distanceText;
    
     public float speed = 5f;
-
+    private bool raceStarted;
 
     private Rigidbody2D rb;
     private Vector2 startPos;
     private Vector2 distance;
+    
+//     Vector3 screenPositionStart;
+//     Vector3 screenPositionEnd;
+//     Vector3 screenPositionDifference;
+//     float timeOffset;
+//     float timeStart;
 
-
-    private void Start()
-    {
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
         startPos = rb.transform.position;
     }
     
-    private void Update()
-    {
-        //rb.AddForce(Vector2.right * speed);
-
-         distance = rb.position - startPos;
-        distanceText.text = "Distance: " + Mathf.FloorToInt(distance.x).ToString("D");                  
+    private void Update() {
+        distance = rb.position - startPos;
+        distanceText.text = "Distance: " + Mathf.FloorToInt(distance.x).ToString("D");       
+        if (!raceStarted) {
+            CheckIfMouseButtonDown();
+            CheckIfMouseButtonUp();
+        }
     }
-    
 
-    public void PushTheCar()
-    {
-        //horizontalInput = Input.GetAxisRaw("Horizontal");
+    void CheckIfMouseButtonDown() {
+        if (Input.GetMouseButtonDown(0)) {
+            // screenPositionStart = Input.mousePosition;
+            // timeStart = Time.time;
+        }
+    }
+
+    void CheckIfMouseButtonUp() {
+        if (Input.GetMouseButtonUp(0)) {
+            // screenPositionEnd = Input.mousePosition;
+            PushTheCar();
+        }
+    }
+
+    public void PushTheCar() {
         rb.AddForce(Vector2.right * (speed * 200));
         Debug.Log("Pushed the car! ");
+        raceStarted = true;
+//      timeOffset = 0.1f;
+//      var timeDifference = Math.Max(Time.time - timeStart, timeOffset);
+//      screenPositionDifference = (screenPositionEnd - screenPositionStart) / Screen.dpi;
+//      var worldDirection = Vector3.forward * screenPositionDifference.y + Vector3.right * screenPositionDifference.x;
+//      rb.AddForce(worldDirection * speed);
     }
 }

6e8cb844b2696f298b166f12e9688185d46967bd
Emil Huzell <65963728+EmilHuzell@users.noreply.github.com>
2021-03-15 13:36:45 +0100

    tests github write access

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//github access test
 public class CameraFollowScript : MonoBehaviour
 {
 private Transform player;

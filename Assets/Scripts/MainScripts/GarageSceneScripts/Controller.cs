5b21ec97e4754b3af3c2ab8db2c045cc00e97e65
Victorpetts <vike.pet@hotmail.com>
2021-03-17 20:06:35 +0100

    Makes a gacha machine that's a 3D object. Makes separate functions and weight tables for the two gachas.

--- Assets/Scripts/MainScripts/GarageSceneScripts/Controller.cs
+++ Assets/Scripts/MainScripts/GarageSceneScripts/Controller.cs
@@ -6,13 +6,16 @@ public class Controller : MonoBehaviour {
     public InventoryObject inventory;
     public PlayerModel player;
     
-    public int[] weights = {50, 40, 30, 20, 10, 1};
+    public int[] weightsSoft = {50, 40, 30, 20, 10, 1};
+    public int[] weightsHard = {30, 30, 30, 30, 10, 10};
 
     public int rollCost = 20;
     public int rewardAmount = 3;
     private float posX;
+    private Camera _camera;
 
     void Start() {
+        _camera = Camera.main;
         var moneyWon = GameObject.Find("MoneyWon");
         if (moneyWon != null) {
             moneyWon.GetComponent<MoneyWon>().AddToStash();
@@ -20,32 +23,58 @@ public class Controller : MonoBehaviour {
     }
     public void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            
-            if (Physics.Raycast(ray, out hit)) {
-                var item = hit.collider.GetComponent<Item>();
-                if (item) {
-                    inventory.AddItem(item.item, 1);
-                    Destroy(hit.collider.gameObject);
+                var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out var hit)) {
+                    var item = hit.collider.GetComponent<Item>();
+                    var gButton = hit.collider.CompareTag("GachaButton");
+                
+                    if (item) {
+                        inventory.AddItem(item.item, 1);
+                        Destroy(hit.collider.gameObject);
+                    }
+                    if (gButton) {
+                        RollGachaSoft();
+                    }
                 }
-            }
         }
     }
 
-    public void RollGacha() {
+    public void RollGachaSoft() {
         if (!player.HasEnoughGold(rollCost)) return;
         player.Gold -= rollCost;
         posX = -1.5f;
         for (int j = 0; j < rewardAmount; j++) {
-            var totalWeights = weights.Sum();
+            var totalWeights = weightsSoft.Sum();
             var random = Random.Range(0, totalWeights);
 
-            var total = weights[0];
+            var total = weightsSoft[0];
             var i = 0;
             while (total < random) { 
                 i++; 
-                total += weights[i];
+                total += weightsSoft[i];
+            }
+            
+            Debug.Log(player.gachaLootTable[i].name);
+            Instantiate(player.gachaLootTable[i], new Vector3(posX, 0, 0), Quaternion.identity);
+            posX += 1.5f;
+        }
+    }
+    
+    public void RollGachaHard() {
+        if (!player.HasEnoughGold(rollCost)) return;
+        // Change type of currency
+        player.Gold -= rollCost;
+        posX = -1.5f;
+        for (int j = 0; j < rewardAmount; j++) {
+            var totalWeights = weightsHard.Sum();
+            var random = Random.Range(0, totalWeights);
+
+            var total = weightsHard[0];
+            var i = 0;
+            while (total < random) { 
+                i++; 
+                total += weightsHard[i];
             }
             
             Debug.Log(player.gachaLootTable[i].name);

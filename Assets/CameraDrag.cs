6e045adc201329d21302f3c38f17dddae35f1579
Victorpetts <vike.pet@hotmail.com>
2021-03-26 13:46:24 +0100

    Camera drag is on a button

--- Assets/CameraDrag.cs
+++ Assets/CameraDrag.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
  
-public class CameraDrag : MonoBehaviour
-{
+public class CameraDrag : MonoBehaviour {
     public float dragSpeed = 2;
     private Vector3 dragOrigin;
+    private bool move;
     
-    void Update() {
-        if (Input.GetMouseButtonDown(0))
-        {
-            dragOrigin = Input.mousePosition;
-            return;
+    public void MoveCam() {
+        // if (Input.GetMouseButtonDown(0)) {
+        //     dragOrigin = Input.mousePosition;
+        //     return;
+        // }
+        //
+        // if (!Input.GetMouseButton(0)) return;
+        //
+        // Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        move = !move;
+        if (move) {
+            Vector3 move = new Vector3(dragSpeed, 0, 0);
+     
+            transform.Translate(move, Space.World);
         }
- 
-        if (!Input.GetMouseButton(0)) return;
- 
-        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        Vector3 move = new Vector3(-pos.x * dragSpeed, 0, 0);
- 
-        transform.Translate(move, Space.World);  
+        else {
+            Vector3 move = new Vector3(-dragSpeed, 0, 0);
+     
+            transform.Translate(move, Space.World);
+        }
     }
- 
- 
+    
 }

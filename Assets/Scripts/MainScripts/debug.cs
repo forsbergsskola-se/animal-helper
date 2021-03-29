05904f019b404f9faea1eeed0206c132b9e142d7
Oskar Jansson <oskar_slim89@hotmail.com>
2021-03-29 17:10:20 +0200

    testing things in SplashScreen

--- Assets/Scripts/MainScripts/debug.cs
+++ Assets/Scripts/MainScripts/debug.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SplashScreenClickToContinue : MonoBehaviour
+public class debug : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,9 +13,11 @@ public class SplashScreenClickToContinue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            SceneManager.LoadScene("GarageScene");
-        }
+        
+    }
+
+    public void debugg()
+    {
+        Debug.Log("button was clicked");
     }
 }

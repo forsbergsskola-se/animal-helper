5f23bc0524400c264538c0e60b759c41ad2f0464
KalleManuel <74299603+KalleManuel@users.noreply.github.com>
2021-04-01 01:05:49 +0200

    more gs

--- Assets/CameraDrag.cs
+++ Assets/CameraDrag.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
  
 public class CameraDrag : MonoBehaviour {
     public bool move;
     public GameObject garageUI;
     public GameObject scrapyardUI;
+    public Text navText;
 
   
 
@@ -13,12 +15,13 @@ public class CameraDrag : MonoBehaviour {
             Vector3 newPos = new Vector3(1.5f, -1.6f, -21);
             scrapyardUI.SetActive(true);
             garageUI.SetActive(false);
+            navText.text = "Go to garage";
 
             transform.position = newPos;
         }
         else {
             Vector3 newPos = new Vector3(11, -1.6f, -21);
-
+            navText.text = "Go to scrapard";
             transform.position = newPos;
             scrapyardUI.SetActive(false);
             garageUI.SetActive(true);

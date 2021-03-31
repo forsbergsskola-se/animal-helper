462937996ca5cf2178f5432c3e2a0cb5e01d096f
KalleManuel <74299603+KalleManuel@users.noreply.github.com>
2021-04-01 00:24:27 +0200

    Im the pusherman

--- Assets/CameraDrag.cs
+++ Assets/CameraDrag.cs
@@ -2,11 +2,17 @@ using UnityEngine;
  
 public class CameraDrag : MonoBehaviour {
     public bool move;
+    public GameObject garageUI;
+    public GameObject scrapyardUI;
+
+  
 
     public void MoveCam() {
         move = !move;
         if (!move) {
             Vector3 newPos = new Vector3(1.5f, -1.6f, -21);
+            scrapyardUI.SetActive(true);
+            garageUI.SetActive(false);
 
             transform.position = newPos;
         }
@@ -14,6 +20,8 @@ public class CameraDrag : MonoBehaviour {
             Vector3 newPos = new Vector3(11, -1.6f, -21);
 
             transform.position = newPos;
+            scrapyardUI.SetActive(false);
+            garageUI.SetActive(true);
         }
     }
 }

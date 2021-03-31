a4087289e5ceac1a9382b9c71c6a497c48eee524
Victorpetts <vike.pet@hotmail.com>
2021-03-31 23:31:22 +0200

    Fixes camera for button

--- Assets/CameraDrag.cs
+++ Assets/CameraDrag.cs
@@ -1,74 +1,19 @@
 using UnityEngine;
  
-public class CameraDrag : MonoBehaviour
-{
-    [SerializeField]
-    private Camera cam;
+public class CameraDrag : MonoBehaviour {
+    public bool move;
 
-    private Vector3 _dragOrigin;
-    private Vector3 _difference;
-
-    private void Update()
-    {
-        PanCamera();
-    }
-
-    private void PanCamera()
-    {
-        // Save position of mouse in world space when drag starts (first time clicked)
-
-        if (Input.GetMouseButtonDown(0))
-            _dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
-
-        // Calculate distance between drag origin and new position if it is still held down
-
-        if (Input.GetMouseButton(0))
-        {
-            _difference = _dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
-            print("Origin " + _dragOrigin + " NewPosition " + cam.ScreenToWorldPoint(Input.mousePosition) + " = Difference " + _difference);
-        }
-      
-        // Move the camera by that distance
-        cam.transform.position += _difference;
-    }
-}
-
-
-
-     /* public float Speed = 5;
-
-    private void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            transform.eulerAngles += Speed * new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
-        }
-    }*/
-
-    /*private Vector3 dragOrigin;
-    //private bool move;
-    
     public void MoveCam() {
-         if (Input.GetMouseButtonDown(0)) {
-            dragOrigin = Input.mousePosition;
-            return;
-        }
-       
-         if (!Input.GetMouseButton(0)) return;
-        
-        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        /*
         move = !move;
-        if (move) {
-            Vector3 move = new Vector3(dragSpeed, 0, 0);
-     
-            transform.Translate(move, Space.World);
+        if (!move) {
+            Vector3 newPos = new Vector3(1.5f, -1.6f, -21);
+
+            transform.position = newPos;
         }
         else {
-            Vector3 move = new Vector3(-dragSpeed, 0, 0);
-     
-            transform.Translate(move, Space.World);
+            Vector3 newPos = new Vector3(11, -1.6f, -21);
+
+            transform.position = newPos;
         }
-       
-    } */
-    
+    }
+}

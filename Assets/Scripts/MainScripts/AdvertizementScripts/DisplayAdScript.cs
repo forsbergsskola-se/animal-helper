4d7f2f63d0e8261b188e0e4b6c9bf9394d380696
Oskar Jansson <oskar_slim89@hotmail.com>
2021-03-31 12:40:35 +0200

    ShowAd button script replaced with Haiyans script.

--- Assets/Scripts/MainScripts/AdvertizementScripts/DisplayAdScript.cs
+++ Assets/Scripts/MainScripts/AdvertizementScripts/DisplayAdScript.cs
@@ -1,65 +1,89 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.Advertisements;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-
-public class DisplayAdScript : MonoBehaviour , IUnityAdsListener
+using UnityEngine.Advertisements;
+public class DisplayAdScript : MonoBehaviour,IUnityAdsListener
 {
+    private string playStoreID = "4068245";
+    private string appleStoreID = "4068244";
 
-    string gameId = "4068245";
-    bool testMode = true;
+    private string interstitialAd = "video";
+    private string rewardedVideoAd = "Rewarded_Android";
 
-    Button myButton;
-    public string mySurfacingId = "rewardedVideo";
-    
-    void Start () {
+    public bool isTargetPlayStore;
+    public bool isTestAd;
 
-        Advertisement.Initialize(gameId, testMode);
-    }
-
-    
-    public void ShowRewardedVideo () {
-        Advertisement.Show (mySurfacingId);
-    }
-    
-    public void ShowInterstitialAd() 
+    private void Start()
     {
-        // Check if UnityAds ready before calling Show method:
-        if (Advertisement.IsReady()) {
-            Advertisement.Show();
-        } 
-        else {
-            Debug.Log("Interstitial ad not ready at the moment! Please try again later!");
-        }
+        Advertisement.AddListener(this);
+        InitializeAdvertisement();
+
+
     }
 
-    public void OnUnityAdsReady(string surfacingId)
+    private void InitializeAdvertisement()
     {
-        // If the ready Ad Unit or legacy Placement is rewarded, activate the button: 
-        if (surfacingId == mySurfacingId) {        
-            myButton.interactable = true;
+        if (isTargetPlayStore)
+        {
+            Advertisement.Initialize(playStoreID, isTestAd);
+            return;
         }
+
+        Advertisement.Initialize(appleStoreID, isTestAd);
+    }
+
+    public void PlayInterstitialAd()
+    {
+        if (!Advertisement.IsReady(interstitialAd))
+        {
+            return;
+        }
+
+        Advertisement.Show(interstitialAd);
+    }
+
+    public void PlayRewardedVideoAd()
+    {
+        if (!Advertisement.IsReady(rewardedVideoAd))
+        {
+            return;
+        }
+
+        Advertisement.Show(rewardedVideoAd);
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+       
     }
 
     public void OnUnityAdsDidError(string message)
     {
-        Debug.Log(message);
+        
     }
 
     public void OnUnityAdsDidStart(string placementId)
     {
-        Debug.Log("user started watching Ad");
+       
     }
 
-    public void OnUnityAdsDidFinish (string surfacingId, ShowResult showResult) {
-    
-        if (showResult == ShowResult.Finished) {
-            // Reward the user for watching the ad to completion.
-        } else if (showResult == ShowResult.Skipped) {
-            // Do not reward the user for skipping the ad.
-        } else if (showResult == ShowResult.Failed) {
-            Debug.LogWarning ("The ad did not finish due to an error.");
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        switch (showResult)
+        {
+            case ShowResult.Failed:
+                break;
+            case ShowResult.Skipped:
+                if(placementId == interstitialAd){Debug.Log("Skipped interstitial");}
+                break;
+            case ShowResult.Finished:
+                if (placementId == rewardedVideoAd)
+                {
+                    Debug.Log("Reward the player");
+                    //TODO add currency
+                    
+                }
+                if(placementId == interstitialAd){Debug.Log("Finished interstitial");}
+                break;
         }
     }
 }

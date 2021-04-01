9a3b7dc843fb950a4fcd83dc6ab58fb4c6c66320
Victorpetts <vike.pet@hotmail.com>
2021-04-01 10:50:44 +0200

    Latest math and moved scenes around

--- Assets/Scripts/AmysFolder/CarStatsMath.cs
+++ Assets/Scripts/AmysFolder/CarStatsMath.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CarStatsMath : MonoBehaviour
@@ -9,17 +7,12 @@ public class CarStatsMath : MonoBehaviour
     public float Drag; // Spoiler
     public float WobbleAmount; // Front Wheel & Back Wheel
 
-    public void Start()
-    {
-    }
     public void Awake()
     {
         StatController();
-
     }
 
-    public void StatController()
-    {
+    public void StatController() {
         var body = HackySave.Body;
         var front = HackySave.Front;
         var wheel = HackySave.Wheel;
@@ -27,16 +20,7 @@ public class CarStatsMath : MonoBehaviour
         PushSpeed = 10 * (front.item.rarityLevel + 200) + (front.level * 150); 
         BoostForce = 10 * ((body.item.rarityLevel * 100) + (body.level * 100) * 10); 
         WobbleAmount = 10 - ((wheel.item.rarityLevel + 1.0f) + (wheel.level * 0.5f));
-        // Drag = 0.8 - ((0.1 * rarity) + (0.1 * item level));
+        Drag = 0.8f - ((0.1f * spoiler.item.rarityLevel) + (0.1f * spoiler.level));
         Drag = 0.5f;
-        /// Need to redo, Level effects more then Rarity.
-        /// 1 - (0.8 * ((4 * 1 ) * ((0.1 + 3) *0.1))) = 0.008 Legendary on Max level.
-        /// 1 - (0.8 * ((1 * 1 ) * ((0.1 + 1) *0.1))) = 0.912 Common on level 0.
-
-        /*
-        if (front.item != null)
-        {
-        }
-        */
     }
 }
